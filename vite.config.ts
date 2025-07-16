6d39302b5b2ca29ad9b9752224ed2f7d20df601d
Swantaan <csjakobus@gmail.com>
2025-07-16 22:12:43 +0200

    wadas

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/dental-website/',
   server: {
     host: "::",
     port: 8080,
